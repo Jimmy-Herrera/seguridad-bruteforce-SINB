7b2ea8a64793c42d47a1071886e552e6de44abdf
Jimmy <jimmyjoeh2006@gmail.com>
2025-09-18 20:49:41 -0500

    Actualizar bruteforce.py con nueva función

--- bruteforce.py
+++ bruteforce.py
@@ -1,6 +1,7 @@
 
 import time
 import matplotlib.pyplot as plt
+import numpy as np
 
 password = input("Ingresa una contraseña de maximo 3 caracteres: ")
 if len(password) == 3:
@@ -43,42 +44,41 @@ elif len(password) <= 2:
     print("La contraseña tiene menos caracteres")
 else:
     print("La contraseña tiene mas caracteres")
-
-# Parte gráfica
+#**************************************************************************
 longitudes = [1, 2, 3]
 tiempos = []
+test_char = alfabeto_completo[-1]
 
 for l in longitudes:
-    test_password = alfabeto_completo[-1]*l
-    intentos = 0
-    encontrada = False
     inicio = time.time()
-
+    test = test_char * l
+    encontrada = False
     for l1 in alfabeto_completo:
-        intentos += 1
-        if l == 1 and l1 == test_password:
+        if l == 1 and l1 == test:
             encontrada = True
             break
         for l2 in alfabeto_completo:
-            intentos += 1
-            if l == 2 and l1 + l2 == test_password:
+            if l == 2 and l1 + l2 == test:
                 encontrada = True
                 break
             for l3 in alfabeto_completo:
-                intentos += 1
-                if l == 3 and l1 + l2 + l3 == test_password:
+                if l == 3 and l1 + l2 + l3 == test:
                     encontrada = True
                     break
             if encontrada:
                 break
         if encontrada:
             break
-
     fin = time.time()
     tiempos.append(fin - inicio)
+x_nuevo = np.linspace(min(longitudes), max(longitudes), 300)
+coef = np.polyfit(longitudes, tiempos, 2)  
+y_nuevo = np.polyval(coef, x_nuevo)
 
-plt.plot(longitudes, tiempos, 'o-')
+plt.plot(x_nuevo, y_nuevo, '-', label="Curva suave")
+plt.scatter(longitudes, tiempos, color='red', label="Puntos reales")
 plt.xlabel("Longitud de la contraseña")
 plt.ylabel("Tiempo (s)")
 plt.title("Tiempo según longitud de la contraseña")
+plt.legend()
 plt.show()
